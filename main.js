0ce87925fde1a85d7aff4f454ab58407b2bd25b3
AlejandroBuitragoB <134160737+AlejandroBuitragoB@users.noreply.github.com>
2023-07-30 19:44:38 -0400

    Todo listo para la primera pre-entrega

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 let nombre = prompt("Ingrese su nombre");
-let apellido = prompt("Ingrese su apellido"); 
-    if (nombre != "" && apellido != ""){
-        alert(`Hola ${nombre} ${apellido}`);
-    }else{
-        alert(`Nombre y apellidos Requeridos`);
-    }
+let apellido = prompt("Ingrese su apellido");
+if (nombre != "" && apellido != "") {
+    alert(`Hola ${nombre} ${apellido}`);
+} else {
+    alert(`Nombre y apellidos Requeridos`);
+}
 
 const dosis = 1.2;
 
@@ -15,15 +15,44 @@ alert(`Debes consumir ${ProteinaNecesaria} gr de proteina al día`);
 
 if (ProteinaNecesaria < 70) {
     alert("Echale un vistazo a nuestros ganadores de masa muscular");
-} else if (ProteinaNecesaria < 100 && ProteinaNecesaria > 70){
+} else if (ProteinaNecesaria < 100 && ProteinaNecesaria > 70) {
     alert("Echale un vistazo a nuestras creatinas")
-} else if (ProteinaNecesaria < 120 && ProteinaNecesaria > 100){
+} else if (ProteinaNecesaria < 120 && ProteinaNecesaria > 100) {
     alert("Aquí podrás encontrar el producto perfecto para ayudarte a definir")
 } else {
-    alert ("Nuestros profesionales pueden guiarte en este camino")
+    alert("Nuestros profesionales pueden guiarte en este camino")
 }
 
+let respuesta = prompt("¿Estás listo para empezar?");
+
+while (respuesta != "si") {
+    alert("¡animos!");
+    respuesta = prompt("¿Estás listo para empezar?");
+}
+
+alert("¡VAMOS!");
 
 
+let meta = prompt(`Entonces ${nombre}, ¿cuál es tu nivel de fitness: bajo, medio o alto?`);
 
+while (meta != "ESC") {
+    switch (meta) {
+        case "bajo":
+            alert("Puedes asesorarte con nuestros expertos");
+            break;
 
+        case "medio":
+            alert("Estamos aquí si tienes alguna duda");
+            break;
+
+        case "alto":
+            alert("Seguro prefieres manejarte solo");
+            break;
+
+        default:
+            alert("si no tienes idea, puedes consultar en el chatbot")
+            break;
+    }
+
+    meta = prompt(`Entonces ${nombre}, ¿cuál es tu nivel de fitness: bajo, medio o alto?`);
+}
