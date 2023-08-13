d1dcfc846357e7b23d466d5d27e282decfca12ac
AlejandroBuitragoB <134160737+AlejandroBuitragoB@users.noreply.github.com>
2023-08-13 19:46:13 -0400

    borrador segunda entrega

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
-/*Interfaz de bienvenida al portal personalizada */
-let nombre = prompt("Ingrese su nombre");
+/*Interfaz de bienvenida al portal personalizada. El usario podrá optar por una atención "guiada" al hacer click en el botón "empecemos" */
+let boton = document.getElementById("boton");
+boton.addEventListener("click", () => {
+
+    let nombre = prompt("Ingrese su nombre");
 let apellido = prompt("Ingrese su apellido");
 if (nombre != "" && apellido != "") {
     alert(`Hola ${nombre} ${apellido}`);
@@ -12,11 +15,11 @@ if (nombre != "" && apellido != "") {
 
 let peso = parseInt(prompt("¿cúal es su peso (en números)?"));
 
-function multip (peso , b = 1.2) {
+function multip(peso, b = 1.2) {
     return peso * b;
 }
 
-const proteinanecesaria = multip (peso);
+const proteinanecesaria = multip(peso);
 
 alert(`Debes consumir ${proteinanecesaria} gr de proteina al día`);
 
@@ -66,29 +69,86 @@ while (meta != "SALIR") {
     meta = prompt(`Entonces ${nombre}, ¿cuál es tu nivel de fitness: bajo, medio o alto?`);
 }
 
+    
+})
+
+
 
 class Producto {
-    constructor(nombrep, precio, imagen) {
-        this.nombrep = nombrep;
+    constructor(nombre, precio, imagen, cantidad) {
+        this.nombre = nombre;
         this.precio = precio;
         this.imagen = imagen;
-        this.vendido = false;
+        this.cantidad = cantidad;
     }
 
     vender() {
-        this.vendido = true;
+        if (this.cantidad === 0) {
+            alert(`no hay mas ${this.nombre} disponible`);
+        } else {
+            this.cantidad -= 1;
+        }
     }
 }
 
-const producto1 = new Producto("proteina1", 22.000, "insertar url de foto de preotina");
-const producto2 = new Producto("proteina2", 19.000, "insertar url de foto de preotina");
-const producto3 = new Producto("proteina3", 18.000, "insertar url de foto de preotina");
-const producto4 = new Producto("proteina4", 17.000, "insertar url de foto de preotina");
-const producto5 = new Producto("creatina1", 25.000, "insertar url de foto de creatina");
-const producto6 = new Producto("creatina2", 26.000, "insertar url de foto de creatina");
-const producto7 = new Producto("barrasp1", 21.000, "insertar url de foto de barras");
-const producto8 = new Producto("barrasp2", 21.000, "insertar url de foto de barras");
-const producto9 = new Producto("barrasp3", 21.000, "insertar url de foto de barras");
-const producto10 = new Producto("barrasp4", 21.000, "insertar url de foto de barras");
+const producto1 = new Producto("proteina1", 22000, "insertar url de foto de proteina", 5);
+const producto2 = new Producto("proteina2", 19000, "insertar url de foto de proteina", 5);
+const producto3 = new Producto("proteina3", 18000, "insertar url de foto de proteina", 5);
+const producto4 = new Producto("proteina4", 17000, "insertar url de foto de proteina", 5);
+const producto5 = new Producto("creatina1", 25000, "insertar url de foto de creatina", 5);
+const producto6 = new Producto("creatina2", 26000, "insertar url de foto de creatina", 5);
+const producto7 = new Producto("barrasp1", 21000, "insertar url de foto de barras", 5);
+const producto8 = new Producto("barrasp2", 21000, "insertar url de foto de barras", 5);
+const producto9 = new Producto("barrasp3", 21000, "insertar url de foto de barras", 5);
+const producto10 = new Producto("barrasp4", 21000, "insertar url de foto de barras", 5);
 
-console.log(producto1);
+const productos = [];
+
+productos.push(new Producto("proteina", 22000));
+productos.push(new Producto("proteina", 19000));
+productos.push(new Producto("proteina", 28000));
+productos.push(new Producto("proteina", 27000));
+productos.push(new Producto("creatina", 25000));
+productos.push(new Producto("creatina", 26000));
+productos.push(new Producto("barras", 5000));
+productos.push(new Producto("barras", 5000));
+productos.push(new Producto("barras", 6000));
+productos.push(new Producto("barras", 4000));
+
+
+let nombrep = prompt("Ingrese el nombre del producto a consultar");
+
+while (nombrep != "SALIR") {
+    let producto;
+
+    for (const item of productos) {
+        if (item.nombre === nombrep) {
+            producto = item;
+        }
+    }
+
+    if (producto) {
+        let mensaje = `
+    nombre: ${producto.nombre}
+    precio: ${producto.precio}
+    `;
+
+        alert(mensaje);
+    } else {
+        alert("El producto no se encuentra disponible");
+    }
+
+    nombrep = prompt("Ingrese el nombre del producto a consultar. Escriba SALIR para busqueda manual");
+}
+
+let precio = parseInt(prompt("Ingrese el precio minimo"));
+let filtrados = productos.filter((item) => item.precio > precio);
+
+filtrados.forEach(item => {
+    let mensaje = `
+    nombre: ${item.nombre}
+    precio: $${item.precio}`
+
+    alert(mensaje)
+})
+
